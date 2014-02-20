fab74bef1e652d5e7bbc82ec239fef6aca3d2df8
luftskepp <luftskepp@gmail.com>
2014-02-20 13:31:16 +0100

    updated main-routine and interrupts

--- LAB2/lab2.s
+++ LAB2/lab2.s
@@ -8,66 +8,101 @@ start:
 	jsr setup-interrupt
 	jsr setup-pia
 
+game-init:
+	move.b #ff,d6		;mark serve
+	move.b #1,d2		;ball at right edge
 
-setup-game:
-	move.b #$ff,d6		;mark serve
-setup-serve:
-	cmp.b #0,d5
-	bne left-serve
-	move.b #1,d2		;right-serve
-	bra game
-left-serve:
-	move.b #128,d2		;left-serve
 game:
+	cmp.b #0,d2		;ball out-of-bounds?
+	beq out-of-bound
+	move.b 500,d0
 	jsr delay
-	cmp.b #0,d6		;check not-serve?
-	bne update-ball
-
-move-ball:
+	jsr update-ball
+	bra game
+	
+out-of-bound:
+	jsr score
+	bra game
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+update-ball:
+	cmp.b #ff,d6		;serve?
+	beq update-led
 	cmp.b #0,d5		;check direction
 	bne move-right
-	lsl.b #1,d2		;move ball left
-	bra update-ball
-move-right:
-	lsr.b #1,d2		;move ball right
-update-ball:
+	lsl.b #1,d2		;move left
+	bra update-led
+move-right
+	lsr.b #1,d2
+update-led:
 	move.b d2,$10080
-	cmp.b #0,d2		;ball out-of-bounds?
-	beq score
-	bra game
-
+	rts
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 score:
 	cmp.b #0,d5		;check direction
 	bne leftscore
 	add.b #1,d4		;add 1 to right score
-	bra setup-game
+	bra score-done
 leftscore:
-	add.b #1,d4		;add 1 to left score
-	bra setup-game
-
-
+	add.b #1,d3		;add 1 to left score
+	bra score-done
+score-done:
+	move.b #$ff,d6		;mark serve
+	rts
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+delay:
+	sub.b #1,d0
+	cmp.b #0,d0
+	bne delay
+	rts
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 setup-pia:
 	move.b #0,$10084	;address DDRA
 	move.b #$ff,$10080	;set port 0-7 for output - ballindicator
 	move.b #5,$10084	;address PIAA and set CRA-7 for interrupt 
 	move.b #0,$10086	;address DDRB
-	;move.b #$ff,$10082	;set 0,1,2,3 as output
+	;move.b #$ff,$10082	;set 0-7 as outputs
 	move.b #5,$10086	;address PIAB and set CRB-7 for interrupt
 	rts
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 setup-interrupt:
 	LEA leftinterrupt,a1
 	LEA rightinterrupt,a2
 	move.l a1,$68
 	move.l a2,$74
 	rts 
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 leftinterrupt:
-	tst.b $10082		;acknowledge lvl2 interrupt
-	cmp.b #0,d6
+	tst.b $10082		;acknowledge lvl2 interrupt	
+	cmp.b #128,d2		;ball at left edge?
+	bne not-left-end	
+	cmp.b #0,d6		;ball in-game?
+	beq switch-right
+	move.b #0,d6		;put ball in-game
+	bra done-left
+switch-right:
+	neg.b d5		;change ball-direction
+	bra done-left
+not-left-end:
+	cmp.b #ff,d6		;right-serve?
+	beq done-left
+	jsr score-right
+done-left:
 	rte
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 rightinterrupt:
 	tst.b $10080		;acknowledge lvl5 interrupt
-
+	cmp.b #1,d2		;ball at right edge?
+	bne not-right-end
+	cmp.b #0,d6		;ball in-game?
+	beq switch-left
+	move.b #0,d6		;put ball in-game
+	bra done-right
+switch-left:
+	neg.b d5
+	bra done-right
+not-right-end:
+	cmp.b #ff,d6		;left-serve?
+	beq done-right		
+	jsr score-left
+done-right:
 	rte
